dcb8e09b99218dcfedf1a333f92f70ad342da881
Ace Data Cloud Dev <dev@acedata.cloud>
2026-02-15 12:17:09 +0800

    style: apply ruff format to all Python files

---
+++ test_ruff_hook.py
@@ -0,0 +1,7 @@
+def ugly_function(x, y, z):
+    result = x + y + z
+    d = {"key1": "value1", "key2": "value2", "key3": "value3"}
+    l = [1, 2, 3, 4, 5]
+    if result > 10:
+        print("big")
+    return result
