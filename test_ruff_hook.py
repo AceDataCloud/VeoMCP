3c58f1c7f05e54429c634b032fafa23ddc25044b
Ace Data Cloud Dev <dev@acedata.cloud>
2026-02-15 12:17:15 +0800

    chore: remove test file

--- test_ruff_hook.py
+++
@@ -1,7 +0,0 @@
-def ugly_function(x, y, z):
-    result = x + y + z
-    d = {"key1": "value1", "key2": "value2", "key3": "value3"}
-    l = [1, 2, 3, 4, 5]
-    if result > 10:
-        print("big")
-    return result
